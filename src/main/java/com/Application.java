b75c72f84e33c326eab5a830e2b76de1364cead0
zhigongzhang <zhigongzhang@192.168.138.1>
2019-03-15 12:48:12 +0800

    首次创建

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,12 +2,19 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
